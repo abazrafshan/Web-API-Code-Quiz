55486d2285d214c05ee0065b451e6d4e05fd1fa5
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-04-27 15:15:09 -0700

    Declared js variables

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+var scores = document.querySelector(".scores");
+var timer = document.querySelector(".timer");
+var time  = document.querySelector("#time");
+var startDiv  = document.querySelector("#start-div");
+var startButton = document.querySelector("#start");
+var questionDiv = document.querySelector("#questions");
+var questionHeader = document.querySelector("#question-header");
+var questionOptions = document.querySelector("#question-options");
+var gameOverDiv = document.querySelector("#game-over");
+var score = document.querySelector("#score");
+var initials = document.querySelector("#initials");
+var submitButton = document.querySelector("#submit");
+var results = document.querySelector("results");
+
+
+function startQuiz(event){}
+
+
+startButton.addEventListener("click", startQuiz);
